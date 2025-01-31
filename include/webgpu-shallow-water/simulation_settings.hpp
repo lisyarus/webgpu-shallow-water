cee38fd15511086a334598fad53bf1ed6ee6601f
lisyarus <lisyarus@gmail.com>
2025-01-31 14:54:33 +0300

    Tweak default simulation dt

--- include/webgpu-shallow-water/simulation_settings.hpp
+++ include/webgpu-shallow-water/simulation_settings.hpp
@@ -4,7 +4,7 @@ struct SimulationSettings
 {
     unsigned int cellsX = 256;
     unsigned int cellsY = 256;
-    float dt = 0.1f;
+    float dt = 0.2f;
     float gravity = 10.f;
     float friction = 0.f;
 
